181f1160bac59cf6b2c9c0d53c3ff52d6d7aefad
JimmyW225 <266764@glenbrook225.org>
2025-11-12 11:38:54 -0600

    nov 12

--- scripts.js
+++ scripts.js
@@ -1,16 +1,44 @@
 const numberDisplay = document.getElementById('myNumber');
 const changeButton = document.getElementById('changeButton');
+const highScoreDisplay = document.getElementById('highScoreDisplay');
 
-
-let currentNumber = 1;
+let currentNumber1 = 1;
+let currentNumber2 = 1;
 
 function updateNumber() {
     // Increment the number (or apply other logic)
-    currentNumber=currentNumber * 2; 
+    currentNumber1=currentNumber1 * 2; 
     
     // Update the display on the webpage
-    numberDisplay.textContent = currentNumber; 
+    numberDisplay.textContent = currentNumber1; 
 }
 
 changeButton.addEventListener('click', updateNumber);
+changeButton.addEventListener('click', checkHighScore);
 
+
+const myButton = document.getElementById('changeButton');
+myButton.addEventListener('click', function() {
+const randomNumber = Math.random();
+
+    // Define the failure threshold (e.g., 0.3 for a 30% chance of failure)
+    const failureThreshold = 0.05;
+
+    // Check if the random number is below the failure threshold
+    if (randomNumber < failureThreshold) {
+        currentNumber1=.5
+    } else {
+        //success
+    }
+});
+
+ 
+function checkHighScore() {
+    
+    if (currentNumber1 > currentNumber2) {
+    currentNumber2 = currentNumber1;
+  }
+  
+  highScoreDisplay.textContent = currentNumber2;
+ 
+}
